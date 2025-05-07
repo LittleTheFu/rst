27ebde9140b115bc2395b4a8fb25cfe259765b44
LittleTheFu <lttlstpd@live.com>
2025-05-07 20:42:51 +0800

    save

--- scene.h
+++ scene.h
@@ -1,7 +1,6 @@
 #ifndef _SCENE_H_
 #define _SCENE_H_
 
-#include <array>
 #include <string>
 #include "camera.h"
 #include "GBufferPass.h"
@@ -18,7 +17,6 @@ private:
     SceneData sceneData_;
     std::unique_ptr<GBufferPass> gBufferPass_;
     Camera camera_;
-    std::array<Vertex, 3> vertices_;
     std::string vertex_shader_;
     std::string fragment_shader_;
     unsigned int shaderProgram_;
