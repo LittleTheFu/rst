c087947984be6d0750e39e7f21ff1f9665e995b1
LittleTheFu <lttlstpd@live.com>
2025-05-10 14:34:44 +0800

    fix stbi_image_free bug

--- texture.cpp
+++ texture.cpp
@@ -54,11 +54,11 @@ bool Texture::loadTextureFromFile(const std::string& path) {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-        stbi_image_free(data_);
+        // stbi_image_free(data_);
         return true;
     } else {
         std::cerr << "ERROR::TEXTURE::Texture failed to load at path: " << path << std::endl;
-        stbi_image_free(data_);
+        // stbi_image_free(data_);
         return false;
     }
 }
