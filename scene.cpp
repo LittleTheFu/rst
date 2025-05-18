113b2a8bada3753f4bf681edef413ec4576371d3
LittleTheFu <lttlstpd@live.com>
2025-05-18 21:41:45 +0800

    show cursor mesh

--- scene.cpp
+++ scene.cpp
@@ -98,7 +98,7 @@ void Scene::init()
     mesh_teapot->setPosition(Eigen::Vector3f(0.0f, 0.0f, 0.0f));
     sceneData_.objects.push_back(std::move(mesh_box));
     sceneData_.objects.push_back(std::move(mesh_teapot));
-    // sceneData_.objects.push_back(std::move(mesh_cursor));
+    sceneData_.objects.push_back(std::move(mesh_cursor));
     sceneData_.skybox = std::move(mesh_sky);
 
     // 5. 初始化光源
