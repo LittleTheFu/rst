d991e4f2caf5abe6ec256c0480fc9aab65d2ff9e
LittleTheFu <lttlstpd@live.com>
2025-06-05 17:02:19 +0800

    add another plane to generate the shadow

--- scene.cpp
+++ scene.cpp
@@ -121,11 +121,18 @@ void Scene::init()
     sceneData_.opaqueObjects.push_back(std::move(mesh_transparent_teapot));
     // sceneData_.transparentObjects.push_back(std::move(mesh_transparent_teapot));
 
-    std::unique_ptr<Mesh> mesh_plane = std::make_unique<Mesh>("plane.obj");
-    mesh_plane->setMaterial(goldMaterial);
-    mesh_plane->setScale(Eigen::Vector3f(7.0f, 7.0f, 12.0f));
-    mesh_plane->setPosition(Eigen::Vector3f(0.0f, -2.0f, -10.0f));
-    sceneData_.opaqueObjects.push_back(std::move(mesh_plane));
+    std::unique_ptr<Mesh> mesh_plane_top = std::make_unique<Mesh>("plane.obj");
+    mesh_plane_top->setMaterial(goldMaterial);
+    mesh_plane_top->setScale(Eigen::Vector3f(7.0f, 7.0f, 12.0f));
+    mesh_plane_top->setPosition(Eigen::Vector3f(0.0f, -2.0f, -10.0f));
+    sceneData_.opaqueObjects.push_back(std::move(mesh_plane_top));
+
+    //a trick to make the plane shadow
+    std::unique_ptr<Mesh> mesh_plane_bottom = std::make_unique<Mesh>("plane.obj");
+    mesh_plane_bottom->setMaterial(goldMaterial);
+    mesh_plane_bottom->setScale(Eigen::Vector3f(-7.0f, 7.0f, 12.0f));
+    mesh_plane_bottom->setPosition(Eigen::Vector3f(0.0f, -2.0f, -10.0f));
+    sceneData_.opaqueObjects.push_back(std::move(mesh_plane_bottom));
 
     std::unique_ptr<Mesh> mesh_cursor = std::make_unique<Mesh>("bx.obj");
     mesh_cursor->setMaterial(goldMaterial);
